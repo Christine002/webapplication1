3104110a1d0c54d3d9998afc9a827768bca3abad
Christine002 <56103318+Christine002@users.noreply.github.com>
2019-12-20 01:32:30 -0500

    Feature: Updated run.py

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
-# /run.py
 import os
-from dotenv import load_dotenv, find_dotenv
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-from src.app import create_app
+from src.app import create_app, db
 
-load_dotenv(find_dotenv())
 
 env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
 
+migrate = Migrate(app=app, db=db)
+
+manager = Manager(app=app)
+
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
-  port = os.getenv('PORT')
-  # run app
-  app.run(host='0.0.0.0', port=port)
+  manager.run()
